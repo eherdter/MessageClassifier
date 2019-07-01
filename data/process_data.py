a65e4d08a9f8ab04ca0a541aad79ad1cb4d862c3
Herdter <eherdter@mail.usf.edu>
2019-07-01 10:54:16 -0400

    feat: Built out clean_data functionality

--- data/process_data.py
+++ data/process_data.py
@@ -22,7 +22,46 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
-    pass
+    
+    '''Tidy and clean df. Tidies the categories columns, cleans up duplicates rows,
+    removes rows with non binary (0/1) category entries, removes rows where there is no category selected (1).'''
+    
+    '''Returns: pandas.DataFrame '''
+    
+    #split categories into separate category columns and assign column names
+    categories = df.categories.str.split(';', expand=True)
+    categories.columns = categories.iloc[0].apply(lambda x: x[:len(x)-2])
+    
+    #convert category values to just binary (0/1)
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
+        categories[column] = categories[column].astype(int)
+    
+    #replace categories column in df with new categories
+    df = df.drop(columns=['categories'])
+    df = pd.concat([categories,df], axis=1)
+    
+    #removes duplicate rows
+    df = df.drop_duplicates()
+    
+    #removes rows where related column is non-binary (e.g. ! (0|1))
+    df = df[df['related'] !=2]
+    
+    #remove rows where there is not selected category marked
+        #make a new sum column to check
+    col_list= list(df.drop([ 'direct_report','message','original','genre'], axis=1))
+    df['sum'] = df[col_list].sum(axis=1)
+    
+    df = df[df['sum'] !=0]
+        #drop the sum column
+    df.drop(['sum'], axis=1, inplace=True)
+    
+    #remove rows with message starting with "NOTES:"
+    df = df[~df.message.str.contains("NOTES:")]
+    
+    return df
+    
 
 
 def save_data(df, database_filename):
