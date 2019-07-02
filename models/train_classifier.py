cd197a4e30dda47a715054245a9fc9ca75b69a33
Herdter <eherdter@mail.usf.edu>
2019-07-02 12:11:59 -0400

    feat: Build out build_model and tokenize function with ML pipeline
    tokenize: normalizes, lemmatizes, removes stop words, stemms
    
    Grid Search params: parameters = {
            'tfidf__use_idf': (True, False),
            'clf__n_estimators':range(50,100,25),
            'clf__min_samples_split':range(5,25,5)
        }

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,12 +1,14 @@
 import sys
 import nltk
-nltk.download(['punkt', 'wordnet'])
+nltk.download(['punkt', 'wordnet', 'stopwords'])
 import re
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine 
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
@@ -14,6 +16,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
+import pickle
 
 
 def load_data(database_filepath):
@@ -40,66 +43,95 @@ def tokenize(text):
     
     for url in detected_urls:
         text = text.replace(url, "urlplaceholder")
+        
+    #Normalize (lowercase and remove punctuation)
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
     
-    #tokenize the text
+    #Tokenize text
     tokens = word_tokenize(text)
     
-    #instantiate the WordNetLammatizer
-    lemmatizer = WordNetLemmatizer()
     
-    #lemmatize the tokens from above
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-        
+    #Lemmatize and remove stopwords, end with stemming
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = [lemmatizer.lemmatize(tok).strip() for tok in tokens if tok not in stopwords.words("english")]
+    clean_tokens = [PorterStemmer().stem(tok) for tok in clean_tokens]
+    
     return clean_tokens
     
 
 def build_model():
     
-    ''' Builds the ML pipleline and performs Grid Search with CV.'''
+    ''' Builds the ML pipeline with GridSearchCV.'''
     ''' Returns: Instantiated model that should be fit in a following function. '''
     
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer = tokenize)),
         ('tfidf', TfidfTransformer()),
-        ('clf', RandomForestClassifier())
+        ('clf', RandomForestClassifier(random_state=42))
     ])
     
     
     parameters = {
-    'clf__n_estimators': [50, 100, 200]
+        'tfidf__use_idf': (True, False),
+        'clf__n_estimators':range(50,100,25),
+        'clf__min_samples_split':range(5,25,5)
     }
 
-    cv = GridSearchCV(pipeline, param_grid = parameters)
+    model = GridSearchCV(pipeline, param_grid = parameters)
     
-    return cv
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    
+    '''Makes a prediction and evaluates the models predictive abilities using a
+        classification_report scheme.'''
+    
+    '''Prints: Classification report where reported averages are a 
+        prevalence-weighted macro-average across classes.'''
+    
+    y_pred = model.predict(X_test)
+    
+    #loops through each category and prints classification report for each.
+    for i in range(len(y_pred.T)):
+        cat = category_names[i]
+        pred_cat = y_pred.T[i]
+        test_cat = Y_test.T[i]
+        print(cat, classification_report(test_cat, pred_cat)) #adjust this
+    
+    return None
+ 
 
 
 def save_model(model, model_filepath):
-    pass
+    
+    ''' Saves model to pickle file.'''
+    
+    filename = model_filepath
+    pickle.dump(model, open(filename, 'wb'))
 
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
+        
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
+        
+        print('Splitting data into testing and training sets using test_size = 0.2')
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
         print('Building model...')
         model = build_model()
+        print('Finished  building model.')
         
         print('Training model...')
         model.fit(X_train, Y_train)
+        print('Finished training model.')
         
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
+        print('Finished evaluating model.')
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
